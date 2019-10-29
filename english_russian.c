5c1fc69b03756bd540aacf0d977317f1f1ae7701
LFR1 <2819688628@qq.com>
2019-10-29 10:19:55 +0800

    max_5_int add commit

--- english_russian.c
+++ english_russian.c
@@ -1,8 +1,18 @@
-//一个旅行社有 72 人，其中会英语的有 48 人，会俄语的有 36 人，两样都不会的有 8 人，两样都会的有
-//_______个。
-//编程：输入旅行社有72人
-//     输入会英语48人
-//     输入会俄语36人
-//     输入两样都不会有8人
-//     输出两样都会的有   人 
-//     输出：输入数据错误，无法计算
+#include<stdio.h>
+main()
+{
+   	int people, english, russian, allcan, allcannot;
+   	printf("�����ùݹ��ж�����\n");
+   	scanf("%d",&people);
+   	printf("�����Ӣ����ж�����\n");
+   	scanf("%d",&english);
+   	printf("����������ж�����\n");
+   	scanf("%d",&russian);
+   	printf("�������߶�������ж�����\n");
+   	scanf("%d",&allcannot);
+   	allcan = english + russian +allcannot - people;
+   	if(allcan >= 0)
+   	printf("���߶������%d��\n",allcan);
+   	else
+   	printf("�������ݴ����޷�����\n");
+} 
